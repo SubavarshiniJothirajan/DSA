13bf0bd177ff1269f6b7ce3b0fbe33a2dfc8a478
SubavarshiniJothirajan <162786248+SubavarshiniJothirajan@users.noreply.github.com>
2026-05-23 19:10:10 +0530

    Implement setZeroes method

---
+++ Arrays/SetMatrixZero.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void setZeroes(int[][] mat) {
+        int[] row = new int[mat.length];
+        int[] col = new int[mat[0].length];
+
+        for(int i=0;i<mat.length;i++)
+        {
+            for(int j=0;j<mat[0].length;j++)
+            {
+                if(mat[i][j]==0)
+                {
+                    row[i] = 1;
+                    col[j] = 1;
+                 }
+                
+            }
+        }
+         for(int i=0;i<mat.length;i++)
+        {
+            for(int j=0;j<mat[0].length;j++)
+            {
+                if(row[i]==1||col[j]==1)
+                {
+                    mat[i][j] = 0;
+                }
+            }
+        }
+    }
+}
