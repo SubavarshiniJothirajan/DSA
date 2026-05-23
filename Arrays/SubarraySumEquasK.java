c03cb182627ab2198b92e7c8658de637ace49c13
SubavarshiniJothirajan <162786248+SubavarshiniJothirajan@users.noreply.github.com>
2026-05-23 19:13:07 +0530

    Implemented Countnumberofsubarraywithsum K method
    Implement a solution to find the number of subarrays that sum to a given value k.

---
+++ Arrays/SubarraySumEquasK.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int subarraySum(int[] arr, int k) {
+        int count = 0,sum=0;
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int i=0;i<arr.length;i++)
+        {
+            sum+=arr[i];
+            if(map.containsKey(sum-k))
+                count+=map.get(sum-k);
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
+}
