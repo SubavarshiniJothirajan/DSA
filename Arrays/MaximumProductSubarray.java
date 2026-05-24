fde8d9b1302941ef064ccc5170c70e011871a1bc
SubavarshiniJothirajan <162786248+SubavarshiniJothirajan@users.noreply.github.com>
2026-05-24 20:07:55 +0530

    Implement Maximum Product Subarray

---
+++ Arrays/MaximumProductSubarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int prefix=1,suffix=1;
+        int max = Integer.MIN_VALUE;
+        int n = nums.length;
+        if(nums.length==1)
+            return nums[0];
+        for(int i=0;i<nums.length;i++)
+        {
+            
+            if(prefix==0)prefix=1;
+            if(suffix==0)suffix=1;
+            prefix*=nums[i];
+            suffix*=nums[n-i-1];
+            max = Math.max(max,Math.max(prefix,suffix));
+        }
+       
+        return max;
+    }
+}
